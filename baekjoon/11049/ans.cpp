4d9d3b22f3ed92d794c50e3fd2f45a3592a96097
JH-Ha <cubeprince@gmail.com>
2020-09-20 23:25:24 +0900

    [baekjoon] remove useless code at 11049

--- baekjoon/11049/ans.cpp
+++ baekjoon/11049/ans.cpp
@@ -19,16 +19,7 @@ int main()
         a.push_back(make_pair(l, r));
         dp[i][i] = 0;
     }
-    for (int i = 0; i < n - 1; i++)
-    {
-        dp[i][i + 1] = a[i].first * a[i].second * a[i + 1].second;
-    }
-    for (int i = 0; i < n - 2; i++)
-    {
-        dp[i][i + 2] = dp[i][i + 1] + a[i].first * a[i + 2].first * a[i + 2].second;
-        dp[i][i + 2] = a[i].first * a[i].second * a[i + 1].second + dp[i + 1][i + 2];
-    }
-    for (int k = 2; k < n; k++)
+    for (int k = 1; k < n; k++)
     {
         for (int i = 0; i + k < n; i++)
         {
@@ -38,8 +29,6 @@ int main()
                 dp[i][i + k] = min(dp[i][i + k - 1 - j] + dp[i + k - j][i + k] + a[i].first * a[i + k - j].first * a[i + k].second,
                                    dp[i][i + k]);
             }
-            // dp[i][i + 3] = dp[i][i + 2] + a[i].first * a[i + 3].first * a[i + 3].second;
-            // dp[i][i + 3] = dp[i][i + 1] + dp[i + 2][i + 3] + a[i].first * a[i + 1].second * a[i + 3].second;
         }
     }
     cout << dp[0][n - 1] << endl;
