9f1d58378217140d22cf1d5e57c7ada784e647eb
JH-Ha <cubeprince@gmail.com>
2020-09-23 23:23:11 +0900

    [baekjoon] string count, solve 1152

---
+++ baekjoon/1152/ans.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    string s;
+    while (cin >> s)
+    {
+        n++;
+    }
+    cout << n << '\n';
+}
