ec4de725b611b72ee14851e647b0e7967042d074
JH-Ha <cubeprince@gmail.com>
2021-03-22 00:04:57 +0900

    [baekjoon] 1107

--- baekjoon/1107/ans.cpp
+++ baekjoon/1107/ans.cpp
@@ -7,6 +7,11 @@ int m;
 bool broken[10];
 bool validateNumber(int num)
 {
+    if (num == 0 && broken[0])
+    {
+        return false;
+    }
+
     while (num > 0)
     {
         if (broken[num % 10])
@@ -16,6 +21,20 @@ bool validateNumber(int num)
         num /= 10;
     }
     return true;
+}
+int getLen(int num)
+{
+    if (num == 0)
+    {
+        return 1;
+    }
+    int len = 0;
+    while (num > 0)
+    {
+        len++;
+        num /= 10;
+    }
+    return len;
 }
 
 int main()
@@ -33,10 +52,16 @@ int main()
         cin >> brokenNumber;
         broken[brokenNumber] = true;
     }
-
+    int ans = abs(n - 100);
     for (int i = 0; i <= 1000000; i++)
     {
         bool isValidNumber = validateNumber(i);
+        if (isValidNumber)
+        {
+            int len = getLen(i);
+            ans = min(ans, abs(i - n) + len);
+        }
     }
+    cout << ans << endl;
     return 0;
 }
