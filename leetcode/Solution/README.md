3f015473a9650e6f45b0fa4dc9fc0b16e4212da8
JH-Ha <cubeprince@gmail.com>
2021-09-23 21:11:34 +0900

    [leetcode] init

---
+++ leetcode/Solution/README.md
@@ -0,0 +1,16 @@
+## Project Information
+
+This project is for leetcode problem solving with Java.
+Firstly, I will start to solve top 100 linked questions.
+Problems are seperated with packages which represent a problem number.
+
+## Folder Structure
+
+The workspace contains two folders by default, where:
+
+- `src`: the folder to maintain sources
+- `lib`: the folder to maintain dependencies
+
+## Dependency Management
+
+The `JAVA DEPENDENCIES` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-pack/blob/master/release-notes/v0.9.0.md#work-with-jar-files-directly).
---
+++ leetcode/Solution/src/p336/App.java
@@ -0,0 +1,78 @@
+package p336;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    class Trie {
+        List<Integer> palindromeIdx;
+        Trie[] children;
+        int wordIdx = -1;
+
+        public Trie() {
+            this.children = new Trie[26];
+            this.palindromeIdx = new ArrayList<>();
+        }
+
+        public void insert(String s, int idx) {
+            if ("".equals(s)) {
+                this.palindromeIdx.add(idx);
+                this.wordIdx = idx;
+            } else {
+                int charIdx = s.charAt(0) - 'a';
+                if (this.children[charIdx] == null) {
+                    this.children[charIdx] = new Trie();
+                }
+                if (checkPalindrome(s)) {
+                    this.palindromeIdx.add(idx);
+                }
+                this.children[charIdx].insert(s.substring(1), idx);
+            }
+            // System.out.println(s.substring(1, 2));
+        }
+
+    }
+
+    public boolean checkPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<List<Integer>> palindromePairs(String[] words) {
+        Trie root = new Trie();
+        // Trie reRoot = new Trie();
+        for (int i = 0; i < words.length; i++) {
+            root.insert(words[i], i);
+            // reRoot.insert(words[i], i);
+        }
+        Trie node = root;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder builder = new StringBuilder(words[i]);
+            String reWord = builder.reverse().toString();
+            for (int j = 0; j < reWord.length(); j++) {
+                if (node.wordIdx != -1 && checkPalindrome(reWord.substring(j))) {
+                    List<Integer> a1 = new ArrayList<>();
+                    a1.add(node.wordIdx);
+                    a1.add(i);
+                    ans.add(a1);
+                }
+                if (node.children[reWord.charAt(j) - 'a'] == null)
+                    break;
+                node = node.children[reWord.charAt(j) - 'a'];
+            }
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String[] words = { "abcd", "dcba", "lls", "s", "sssll" };
+        App solution = new App();
+        solution.palindromePairs(words);
+    }
+}
