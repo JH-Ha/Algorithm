485f5160e1e45cf204c1361c101c0821d9ee4d02
JH-Ha <cubeprince@gmail.com>
2021-09-27 21:02:31 +0900

    [programmers] weekly challenge 8th week

---
+++ programmers/2021weeklyTest/Java/src/weeklyChallenge2021/eightWeek/App.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int n = sizes.length;
+        int maxWidth = 0;
+        int maxHeight = 0;
+        for(int i = 0; i < n; i ++){
+            maxWidth = Math.max(maxWidth, Math.max(sizes[i][0], sizes[i][1]));
+            maxHeight = Math.max(maxHeight, Math.min(sizes[i][0], sizes[i][1]));
+        }
+        answer = maxHeight * maxWidth;
+        return answer;
+    }
+}
