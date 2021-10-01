37c532617de650852246506a9c2225694471d358
JH-Ha <cubeprince@gmail.com>
2021-10-01 23:23:03 +0900

    [leetcode] p32,p33

---
+++ leetcode/Solution/src/p0032/App.java
@@ -0,0 +1,16 @@
+package p0032;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+
+        return 0;
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "(()";
+        System.out.println(solution.longestValidParentheses(s));
+    }
+}
---
+++ leetcode/Solution/src/p0033/App.java
@@ -0,0 +1,16 @@
+package p0033;
+
+class Solution {
+    public int search(int[] nums, int target) {
+
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0;
+        System.out.println(solution.search(nums, target));
+    }
+}
