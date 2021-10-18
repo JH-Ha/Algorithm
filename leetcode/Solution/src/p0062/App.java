fb2f4b39d10b922a0913e20672e7993b15905759
JH-Ha <cubeprince@gmail.com>
2021-10-19 01:25:58 +0900

    [leetcode] p62

--- leetcode/Solution/src/p0062/App.java
+++ leetcode/Solution/src/p0062/App.java
@@ -2,12 +2,33 @@ package p0062;
 
 class Solution {
     public int uniquePaths(int m, int n) {
+        m = m - 1;
+        n = n - 1;
+        long answer = 1;
+        long up = 1;
+        long leftBottom = 1;
+        long rightBottom = 1;
 
+        while (up <= (m + n) || leftBottom <= m || rightBottom <= n) {
+            if (up <= (m + n)) {
+                answer *= up;
+                up++;
+            }
+            if (leftBottom <= m && answer % leftBottom == 0) {
+                answer /= leftBottom;
+                leftBottom++;
+            }
+            if (rightBottom <= n && answer % rightBottom == 0) {
+                answer /= rightBottom;
+                rightBottom++;
+            }
+        }
+        return (int) answer;
     }
 }
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println(new Solution().uniquePaths(3, 2));
     }
 }
