968d9e1f9a5115d53fd3788632ac14495cd427cf
JH-Ha <cubeprince@gmail.com>
2022-09-12 16:44:36 +0900

    [leetcode] 2, c++

---
+++ leetcode/C_Solution/0002/answer.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *ans = new ListNode();
+        ListNode *cur = ans;
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL || carry != 0)
+        {
+            int sum = 0;
+            if (l1 != NULL)
+            {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL)
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            cur->next = new ListNode(sum % 10);
+            cur = cur->next;
+        }
+
+        return ans->next;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
