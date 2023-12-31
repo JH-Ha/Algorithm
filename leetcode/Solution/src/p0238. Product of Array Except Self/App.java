453cde3621104b2b132026c7a67ed0042e3b122c
JH-Ha <cubeprince@gmail.com>
2023-12-31 22:47:38 +0900

    [leetcode][Java] 238. Product of Array Except Self
    Use `O(1)` extra space

--- leetcode/Solution/src/p0238. Product of Array Except Self/App.java
+++ leetcode/Solution/src/p0238. Product of Array Except Self/App.java
@@ -1,34 +1,69 @@
 package p0238;
 
+// use extra space
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int[] left = new int[nums.length];
+//         int[] right = new int[nums.length];
+//         int product = 1;
+//         for(int i = 0; i < nums.length; i ++){
+//             product *= nums[i];
+//             left[i] = product;
+//         }
+//         product = 1;
+//         for(int i = nums.length - 1; i >= 0; i --){
+//             product *= nums[i];
+//             right[i] = product;
+//         }
+//         int[] answer = new int[nums.length];
+//         answer[0] = right[1];
+//         answer[nums.length - 1] = left[nums.length - 2];
+//         for(int i = 1; i < nums.length - 1; i ++){
+//             answer[i] = left[i -1] * right[i+ 1];
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
+        int zeroCnt = 0;
         int product = 1;
-        for(int i = 0; i < nums.length; i ++){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeroCnt++;
+            }
             product *= nums[i];
-            left[i] = product;
         }
-        product = 1;
-        for(int i = nums.length - 1; i >= 0; i --){
-            product *= nums[i];
-            right[i] = product;
+        if(zeroCnt == 0){
+            for(int i = 0; i < nums.length; i ++){
+                nums[i] = product / nums[i];
+            }
+        } else if(zeroCnt == 1){
+            product = 1;
+            int zeroPos = 0;
+            for(int i = 0; i < nums.length; i ++){
+                if(nums[i] == 0){
+                    zeroPos = i;
+                }else{
+                    product *= nums[i];
+                    nums[i] = 0;
+                }
+            }
+            nums[zeroPos] = product;
+        } else{
+            for(int i = 0; i < nums.length; i ++){
+                nums[i] = 0;
+            }
         }
-        int[] answer = new int[nums.length];
-        answer[0] = right[1];
-        answer[nums.length - 1] = left[nums.length - 2];
-        for(int i = 1; i < nums.length - 1; i ++){
-            answer[i] = left[i -1] * right[i+ 1];
-        }
-        return answer;
+
+        return nums;
     }
 }
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
-    
+
 }
-
-
