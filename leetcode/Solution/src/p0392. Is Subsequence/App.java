58e64b73274f730f82011eb4071b61f9a77356f3
JH-Ha <cubeprince@gmail.com>
2024-01-05 23:52:10 +0900

    [leetcode][Java] 392. Is Subsequence
    Runtime 2 ms Beats 35.97%
    Memory 41.2 MB Beats 29.53%

---
+++ leetcode/Solution/src/p0392. Is Subsequence/App.java
@@ -0,0 +1,29 @@
+package p0392;
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int si = 0;
+        int ti = 0;
+        while (si < s.length() && ti < t.length()) {
+            if (s.charAt(si) == t.charAt(ti)) {
+                si++;
+                ti++;
+            } else {
+                ti++;
+            }
+        }
+        if (si == s.length())
+
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+
+}
